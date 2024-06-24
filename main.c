cace2cb136f5b714b75e6e7e263e3c278b805b66
ademnajjar48 <cctestingportugal@gmail.com>
2024-06-24 16:24:27 +0100

    Create three integer pointers (hour, min, sec)

--- main.c
+++ main.c
@@ -2,6 +2,10 @@
 #include <stdlib.h>
 
 int main() {
-	
+	int* hour = (int*) malloc(sizeof(int));
+	int* min = (int*) malloc(sizeof(int));
+	int* sec = (int*) malloc(sizeof(int));
+
+
 	return 0;
 }
